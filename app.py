9bba87b4a40fb862d4cc2b00ba449351595245c1
nickvogel <nick0vogel@gmail.com>
2022-04-19 10:42:56 -0400

    adding full gitignore, moving 'secret' keys (it's all development anyway, not a real project)

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from private import secret_key, db_name
+
 
 # initialize flask project
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'this_is_only_for_dev'
+app.config['SECRET_KEY'] = secret_key
 csrf = CSRFProtect(app)
 
 #initialize db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_name
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # set up alembic for database updates
---
+++ private.py
@@ -0,0 +1,2 @@
+db_name = 'sqlite:///todo.db'
+secret_key = 'this_is_only_for_dev'
