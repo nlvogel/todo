1e09f2697604d566217db4d21486f2fcb959d1a9
nickvogel <nick0vogel@gmail.com>
2022-04-19 10:49:44 -0400

    deleted "private.py" because git didn't ignore it. I know it will show up in version history, but that's okay since this isn't a real project. Otherwise, I wouldn't have submitted actual keys to github :)

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from private import secret_key, db_name
+import os
 
 
 # initialize flask project
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secret_key
+app.config['SECRET_KEY'] = os.environ["secret_key"]
 csrf = CSRFProtect(app)
 
 #initialize db
-app.config['SQLALCHEMY_DATABASE_URI'] = db_name
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["db_name"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # set up alembic for database updates
--- private.py
+++
@@ -1,2 +0,0 @@
-db_name = 'sqlite:///todo.db'
-secret_key = 'this_is_only_for_dev'
